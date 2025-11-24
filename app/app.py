35e9aa21688bdde98544632223504e1aec05921d
Sebastien Monel <sebastien.monel@gmail.com>
2025-11-24 08:58:04 +0000

    Encrypt and save token in config file Open and decrypt token from file

--- app/app.py
+++ app/app.py
@@ -6,10 +6,14 @@ from flask import (
 from werkzeug.utils import secure_filename
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
-from cryptography.exceptions import InvalidKey
 import os
 import sys
 import json
+import math
+
+#Errors :
+from cryptography.exceptions import InvalidKey
+from json.decoder import JSONDecodeError
 
 UPLOAD_FOLDER = '/uploaded_files'
 #ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'py', 'exe', 'ipynb', 'zip', 'tar', 'sh', ''}
@@ -18,35 +22,68 @@ app = Flask(__name__, static_url_path="")
 app.secret_key = os.urandom(32)  # Used for session.
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def save_config():
+def save_config_file(config):
+    token = ''
+    if 'key' in config :
+        b_token = bytes(config['token'], 'utf-8')
+        token = encrypt(
+                b_token.ljust(math.trunc(len(b_token) / 16) + 16, b'\00'), #.zfill(16),
+                config['key'],
+                config['iv']
+            ).hex()
+
+    data = {
+        'salt' : config['salt'].hex(), #8*16=128
+        'iv' : config['iv'].hex(), #8*16=128
+        'token_encrypted' : token,
+        'neo4j' : {
+            'instance' : config['neo4j']['instance'],
+            'login' : config['neo4j']['login'],
+            'password_encrypted' : config['neo4j']['password'] #TO_REVIEW
+            }
+        }
+
+    with open('/config/config.json', mode='wt') as config_file:
+        json.dump(data, config_file)
+    return None
+
+def open_config_file(key=b""):
     data = {}
-    data['salt'] = config['salt'].hex() #8*16=128
-    data['iv'] = config['iv'].hex() #8*16=128
-    #config['token'] = os.urandom(32).hex() #8*32=256
+    with open('/config/config.json', mode='rt') as config_file:
+        try:
+            tempo = json.load(config_file)
+        finally :
+            pass
 
-    config_file = open('/config/config.json', mode='wt')
-    json.dump(config, config_file)
-    config_file.close()
+    token = ''
+    if key :
+        token = decrypt(bytes.fromhex(tempo['token_encrypted']), key, bytes.fromhex(tempo['iv'])).rstrip(b'\x00').decode('utf-8')
 
-
+    return {
+        'salt': bytes.fromhex(tempo['salt']),
+        'iv': bytes.fromhex(tempo['iv']),
+        'token': token, #TO_REMOVE
+        'neo4j': {
+            'instance': tempo['neo4j']['instance'],
+            'login': tempo['neo4j']['login'],
+            'password': tempo['neo4j']['password_encrypted']
+            }
+        }
 
 #argon2 et aes config
 if not(os.path.exists('/config/config.json')):
-    config = {}
-    config['salt'] = os.urandom(16).hex() #8*16=128
-    config['iv'] = os.urandom(16).hex() #8*16=128
-    config['token'] = "" #os.urandom(32).hex() #8*32=256
-    config_file = open('/config/config.json', mode='wt')
-    json.dump(config, config_file)
-    config_file.close()
+    save_config_file({
+        'salt' : os.urandom(16), #8*16=128
+        'iv' : os.urandom(16), #8*16=128
+        'token' : '', #os.urandom(32).hex() #8*32=256
+        'neo4j': {
+            'instance': '',
+            'login': '',
+            'password': ''
+        }
+    })
 
-config_file = open('/config/config.json', mode='rt')
-config = json.load(config_file)
-config_file.close()
-
-config['salt'] = bytes.fromhex(config['salt'])
-config['iv'] = bytes.fromhex(config['iv'])
-#config['token'] = "" #TO_REMOVE
+config = open_config_file()
 
 def derive(password): #b"my great password"
     kdf = Argon2id(
@@ -98,47 +135,33 @@ def route_upload_file_get():
         else :
             return render_template('ask_password.html', title='Ask password')
 
-@app.route('/config', methods = ['GET', 'POST'])
+@app.route('/config.json', methods = ['GET', 'POST'])
 def route_download_config():
     if ( (len(request.values) != 0) and ('password' in request.values) ):
         data = {}
-        if 'password' in config :
-            data['password'] = config['password']
-        #data['token'] = config['token']
-        #data['token'] = b"a secret message".hex() ... 8*16
-        #data['token'] = b"tempo".zfill(16).hex()
-        data['token'] = config['token']
-        data['sizeof_token'] = sys.getsizeof(config['token'])
 
-        config['password_b'] = bytes(request.values['password'], 'utf-8')
         try :
-            config['derived'] = derive(config['password_b'])
+            #password_b = bytes(request.values['password'], 'utf-8')
+            #key = derive(password_b)
+            tempo = open_config_file(config['key'])
+
+            data = {
+                'token' : tempo['token']
+                }
+
+        except KeyError:
+            pass
         finally :
             pass
 
-        try :
-            #config['encrypted_token'] = encrypt(bytes(config['token'],'utf-8'), config['derived'], config['iv'])
-            config['encrypted_token'] = encrypt(b"tempo".zfill(16), config['derived'], config['iv'])
-        finally :
-            pass
-
-        try :
-            config['decrypted_token'] = decrypt(config['encrypted_token'], config['derived'], config['iv'])
-        finally :
-            pass
-
-        #if 'key' in config:
-        #    data['verify'] = verify(bytes(config['password'], 'utf-8'), config['key'])
-        #    data['verify'] = verify(bytes(config['password'], 'utf-8'), config['key'])
-        #    data['verify_empty_pwd'] = verify(b"", config['key'])
-        #    data['verify_wrong_pwd'] = verify(b"pas le bon password", config['key'])
-
-        for dict_key in ['key', 'iv', 'salt', 'password_b', 'derived', 'encrypted_token', 'decrypted_token']:
-            if dict_key in config :
-                try :
-                    data[dict_key] = config[dict_key].hex()
-                finally:
-                    pass
+        #for dict_key in ['key', 'iv', 'salt', 'password_b', 'derived', 'encrypted_token', 'decrypted_token']:
+        #    if dict_key in config :
+        #        try :
+        #            data[dict_key] = config[dict_key].hex()
+        #        except KeyError:
+        #            pass
+        #        finally:
+        #            pass
 
         return jsonify(data)
 
@@ -175,6 +198,7 @@ def route_upload_file_post():
             #if ( (len(request.values) != 0) and ('token' in request.values) and (request.values['token'] != "") ):
             if ( 'token' in request.values ):
                 config['token'] = request.values['token']
+                save_config_file(config)
                 return render_template('simple_uploader.html', title='Upload file')
             else:
                 return render_template('ask_token.html', title='Ask token')
