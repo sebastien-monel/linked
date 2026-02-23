7280ccd74979200fe73f66fb25fc98b2f17f824f
sebastien-monel <sebastien.monel@gmail.com>
2026-02-23 13:52:31 +0100

    Add notifications.json import

--- app/templates/notification.js
+++ app/templates/notification.js
@@ -8,6 +8,7 @@ Notification.requestPermission().then((status) => {
   
   if (status === "granted") {
     const interval = setInterval(() => {
+      const data = await import( "/notifications.json",{with:{type:"json}});
       const notification = new Notification("Brouillon :", {body: "Hi there !!!", tag : "1"} );
     }, 1000)
   }
