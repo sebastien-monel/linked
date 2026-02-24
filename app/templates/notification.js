6d0e5e7fad674d2fdf83ff0d9e193e3a84aa08f4
sebastien-monel <sebastien.monel@gmail.com>
2026-02-24 10:49:36 +0100

    Test async function in interval

--- app/templates/notification.js
+++ app/templates/notification.js
@@ -7,31 +7,33 @@ Notification.requestPermission().then((status) => {
   //} 
   
   if (status === "granted") {
-    const interval = setInterval(() => {
-      const response = fetch("/notifications.json");
-      
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-
-      const result = response.json();
-      
-      console.log(typeof result); 
-      console.log(result); 
-      
-      const notifications_json = import( "/notifications.json", { with: { type: "json" } } );
-      const notifications_tab = [] ;
-      
-      console.log(typeof notifications_json); 
-      console.log(notifications_json); 
-
-      //notifications_json.forEach((item, index) => {
-      for (const notification of notifications_json) {
-          notifications_tab.push( new Notification("Brouillon :", {
-            body: notification['body'], 
-            tag : notification['tag']} 
-          ))
-      }
-    }, 1000)
+    const interval = setInterval(notificaiton_check() , 1000)
   }
 })
+
+async notificaiton_check() {
+  const response = await fetch("/notifications.json");
+  
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+  
+  const result = await response.json();
+  
+  console.log(typeof result); 
+  console.log(result); 
+  
+  const notifications_json = import( "/notifications.json", { with: { type: "json" } } );
+  const notifications_tab = [] ;
+  
+  console.log(typeof notifications_json); 
+  console.log(notifications_json); 
+  
+  //notifications_json.forEach((item, index) => {
+  for (const notification of notifications_json) {
+      notifications_tab.push( new Notification("Brouillon :", {
+        body: notification['body'], 
+        tag : notification['tag']} 
+      ))
+  }
+}
