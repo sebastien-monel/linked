af3d05c34fa7ffe96f8a908a02be680fceaf0594
sebastien-monel <sebastien.monel@gmail.com>
2026-03-10 10:48:37 +0100

    Add expried certificate test to renew_certs.sh

--- scripts/renew_certs.sh
+++ scripts/renew_certs.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-certbot renew
+TO_RENEW=`sudo certbot certificates | grep EXPIRED | wc -l`
+
+echo "Number of expired certificates : $TO_RENEW"
+
+if [ $TO_RENEW -gt 0 ]; then
+  certbot renew
+fi
